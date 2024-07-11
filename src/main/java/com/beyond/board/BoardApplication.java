7adcbf2246ca358fef89ded743143d8a0e2e2ea5
tteia <tteia.dl@gmail.com>
2024-07-11 18:19:29 +0900

    윈도우 환경 세팅 값으로 변경(yml)  및 redis 설정

--- src/main/java/com/beyond/board/BoardApplication.java
+++ src/main/java/com/beyond/board/BoardApplication.java
@@ -4,7 +4,9 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession // 세션 스토리지로 레디스를 사용하겠다는 어노테이션.
 @EnableBatchProcessing // batch 사용 시 작성해주는 어노테이션.
 @EnableScheduling // 스케줄러 사용 시 작성해주는 어노테이션.
 @SpringBootApplication
