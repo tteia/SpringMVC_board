22fa89a56c1fac79814b5f35c0767099b84ce14b
tteia <tteia.dl@gmail.com>
2024-07-24 14:25:36 +0900

    ADMIN 권한에게만 회원 목록 조회 노출

--- src/main/java/com/beyond/board/author/service/LoginSuccessHandler.java
+++ src/main/java/com/beyond/board/author/service/LoginSuccessHandler.java
@@ -17,6 +17,7 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         HttpSession httpSession = request.getSession();
         // 방법 1. authentication 인증 객체에서 email 정보 추출.
+        // 우리에게는 아이디(로그인) 용도로 email 이 쓰이므로 getName 했을 때 이메일이 나옴.
 //        httpSession.setAttribute("email", authentication.getName());
 
         // 방법 2. SecurityContextHolder 객체에서 authentication 객체를 꺼낸 뒤, email 정보 추출.
